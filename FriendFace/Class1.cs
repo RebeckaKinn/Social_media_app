7120e6cf3e50fb353a2168f6ce3b49bf195c4e5e
RebeckaKinn <rebeckaki@getacademy.no>
2023-03-15 21:48:24 +0100

    created project

---
+++ FriendFace/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace FriendFace
+{
+    public class Account
+    {
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? email { get; set; }
+        public string? password { get; set; }
+
+    }
+
+    public class User
+    {
+        public List<Account> accounts { get; set; } = new List<Account>();
+        public User()
+        {
+            CreateAccountList();
+        }
+
+        public void CreateAccountList()
+        {
+            accounts.Add(new Account { firstName = "Liam", lastName = "Sørensen", email = "liam@email.com", password = "passord123" });
+            accounts.Add(new Account { firstName = "Elisabeth", lastName = "Olsen", email = "elisabeth@email.com", password = "passord123" });
+            accounts.Add(new Account { firstName = "Jørgen", lastName = "Aalvang", email = "jorgen@email.com", password = "123passord" });
+            accounts.Add(new Account { firstName = "Anne Marie", lastName = "Augustus", email = "anne-marie@email.com", password = "123passord" });
+        }
+    }
+}
---
+++ FriendFace/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FriendFace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new();
+            Console.WriteLine("WELCOME TO FRIENDFACE - THE ONLY SOCIAL PLATFORM YOU NEED");
+            Console.WriteLine(user.accounts[0].firstName);
+        }
+    }
+}
